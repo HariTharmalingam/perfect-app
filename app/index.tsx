ac2235f8c7b28dff9ab2c9a2044c95d5b8797eec
Hari <lqjhari@gmail.com>
2024-07-26 21:00:57 +0200

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-unresolved */
+import useUser from "@/hooks/auth/useUser";
+import { Redirect } from "expo-router";
+import Loader from "@/components/loader/loader";
+
+export default function TabsIndex() {
+  const { loading, user } = useUser();
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        //TODO
+        // <Redirect href={!user ? "/(routes)/onboarding" : "/(tabs)"} />
+        <Redirect href={!user ? "/(routes)/login" : "/(tabs)"} />
+      )}
+    </>
+  );
+}
